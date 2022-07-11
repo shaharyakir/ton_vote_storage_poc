939ecaa25196a6584f3ff04ff55e9e278817a5f3
Shahar Yakir <shahar.yakir@gmail.com>
2022-07-11 10:22:04 +0300

    in prog

--- dfile.spec.ts
+++ dfile.spec.ts
@@ -1,3 +1,53 @@
+import { DFile, IPFSHash, IPFSProvider } from "./dfile";
+import { of } from "ipfs-only-hash";
+
+class DummyHushProvider implements IPFSProvider {
+  myData = {};
+
+  async write(data: string): Promise<IPFSHash> {
+    const h = (await of(data)) as IPFSHash;
+    this.myData[h] = data;
+    return h;
+  }
+  read(hash: IPFSHash): Promise<string> {
+    return this.myData[hash];
+  }
+}
+
+type VoteData = {
+  koko: string;
+  moko: string;
+};
+
 describe("dfile", () => {
-  it("does smth", () => {});
+  it("does smth", async () => {
+    const hashProv = new DummyHushProvider();
+    const df = new DFile<VoteData>(null, hashProv);
+    df.appendData({ koko: "hi", moko: "bo" });
+    df.appendData({ koko: "zo", moko: "co" });
+    const x = await df.write();
+    expect(x.hash).toEqual("QmQAuVQWeULpUnFGHDWGBQyUJz2QiQwaiiGdbsCPzD2Jdq");
+    expect(x.contents).toEqual({
+      data: [
+        { koko: "hi", moko: "bo" },
+        { koko: "zo", moko: "co" },
+      ],
+      prev: null,
+    });
+
+    const df2 = new DFile<VoteData>(x.hash, hashProv);
+    df2.appendData({ koko: "2", moko: "3" });
+    const x2 = await df2.write();
+    expect(x2.contents).toEqual({
+      data: [{ koko: "2", moko: "3" }],
+      prev: x.hash,
+    });
+
+    const fullVC = await df2.readMerge();
+    expect(fullVC).toEqual([
+      { koko: "2", moko: "3" },
+      { koko: "hi", moko: "bo" },
+      { koko: "zo", moko: "co" },
+    ]);
+  });
 });
--- dfile.ts
+++ dfile.ts
@@ -1,26 +1,79 @@
-type IPFSHash = string; // TODO
+export type IPFSHash = string; // TODO
 
-export interface HashProvider {
-    hash(data: string): IPFSHash
+export interface IPFSProvider {
+  write(data: string): Promise<IPFSHash>;
+  read(hash: IPFSHash): Promise<string>;
 }
+
+type FileContents<T> = {
+  data: T[];
+  prev: IPFSHash | null;
+};
+
+type WriteResponse<T> = {
+  contents: FileContents<T>;
+  hash: IPFSHash;
+};
 
 // T should be serializable as JSON
 export class DFile<T> {
   #prev: IPFSHash | null;
-  #data: [T?] = [];
+  #data: T[] = [];
+  #ipfsProvider: IPFSProvider;
+  #hash: IPFSHash | null;
 
-  constructor(prev: IPFSHash | null) {
+  constructor(prev: IPFSHash | null, h: IPFSProvider) {
+    this.#ipfsProvider = h;
     this.#prev = prev;
   }
 
-  // Should generate IPFS hash for outstanding data + prev and return the new hash + content?
-  write() {
+  static async from<T>(hash: IPFSHash, h: IPFSProvider): Promise<DFile<T>> {
+    const newDFile = new DFile<T>(null, h);
+    const fc = await newDFile.#readFile(hash);
+    newDFile.#data = fc.data;
+    newDFile.#prev = fc.prev;
+    newDFile.#hash = hash;
+    return newDFile;
+  }
 
+  async #readFile(hash: IPFSHash): Promise<FileContents<T>> {
+    const s = await this.#ipfsProvider.read(hash);
+    return JSON.parse(s) as FileContents<T>;
+  }
+
+  // Should generate IPFS hash for outstanding data + prev and return the new hash + content?
+  async write(): Promise<WriteResponse<T>> {
+    const fileContents: FileContents<T> = {
+      data: this.#data,
+      prev: this.#prev,
+    };
+    const h = await this.#ipfsProvider.write(JSON.stringify(fileContents));
+    this.#hash = h;
+    return {
+      hash: h,
+      contents: fileContents,
+    };
   }
 
   appendData(d: T) {
+    if (this.#hash) throw new Error("File was already locked");
     this.#data.push(d);
   }
+
+  async readMerge(): Promise<T[]> {
+    if (!this.#hash)
+      throw new Error(
+        "Cannot read unlocked files. Write this file first or instantiate using DFile.from"
+      );
+    let contents: T[] = this.#data;
+    let hashToRead: string | null = this.#prev;
+
+    while (hashToRead) {
+      const fc = await this.#readFile(hashToRead);
+      hashToRead = fc.prev;
+      contents = contents.concat(fc.data);
+    }
+
+    return contents;
+  }
 }
-
-
